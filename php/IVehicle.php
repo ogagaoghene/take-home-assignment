72619fea23832d9e9d283bc290af63ff9eedc6c6
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-03-18 19:32:15 +0100

    Add IVehicle.php file

---
+++ php/IVehicle.php
@@ -0,0 +1,79 @@
+<?php 
+interface IVehicle 
+{
+  public function getManufacturer(): string;
+  public function setManufacturer(string $manufacturer): void;
+  public function getModel(): string;
+  public function setModel(string $model): void;
+  public function drive(): void;
+  public function stop(): void;
+}
+
+interface IDataModel 
+{
+  public function getData(): array;
+}
+
+abstract class AbstractVehicle implements IVehicle
+{
+  protected $manufacturer;
+  protected $model;
+
+  public function __construct($manufacturer, $model)
+  {
+    $this->manufacturer = $manufacturer;
+    $this->model = $model;
+  }
+
+  public function getManufacturer(): string 
+  {
+    return $this->manufacturer;
+  }
+
+  public function setManufacturer(string $manufacturer): void
+  {
+    $this->manufacturer = $manufacturer;
+  }
+
+  public function getModel(): string
+  {
+    return $this->model;
+  }
+
+  public function setModel(string $model): void 
+  {
+    $this->model = model;
+  }
+
+  public function drive(): void 
+  { 
+    echo $this->manufacturer . $this->model. "is" . "<driving/stopping>";
+  }
+
+  public function stop(): void 
+  {
+    echo $this->manufacturer . $this->model. "is" . "<driving/stopping>";
+  }
+}
+
+class Car extends AbstractVehicle implements IDataModel
+{
+  public $color;
+  private $data = [];
+
+  public function __construct($color, $manufacturer, $model)
+  {
+    parent::__construct($manufacturer, $model);
+    $this->data[$color] = $color;
+    $this->data[$manufacturer] = $manufacturer;
+    $this->data[$model] = $model;
+  }
+
+  public function getData(): array
+  {
+    return $this->data;
+  }
+}
+
+$car = new Car("Green", "BMW", "2003");
+$car.getData();
