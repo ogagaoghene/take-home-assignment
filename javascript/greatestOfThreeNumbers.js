bbe23774b48859b758d6e1a5b0710c185c610df3
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-05-06 21:52:12 +0100

    Add greatestOfThreeNumbers.js file

--- javascript/greatestOfThreeNumbers.js
+++ javascript/greatestOfThreeNumbers.js
@@ -19,4 +19,18 @@ const greatestOfNumbers = (num1, num2, num3) => {
     }
 }
 
-greatestOfNumbers(13,10,12);
+const largestOfThreeNumbers = (function(num1, num2, num3) {
+    if (num1 >= num2 && num1 >= num3)
+        console.log(num1, "is greater than", num2, "and", num3);
+    else if (num2 >= num1 && num2 >= num3) {
+        console.log(num2, "is greater than", num1, "and", num3);
+    }
+    else if (num3 >= num1 && num3 >= num2) {
+        console.log(num3, "is greater than", num1, "and", num2);
+    }
+    else {
+        console.log("The three numbers are equal");
+    }
+})(13,45,23)
+greatestOfNumbers(10,23,45);
+largestOfThreeNumbers;
