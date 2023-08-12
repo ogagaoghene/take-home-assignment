501183142a36c933666ca6de5b239ae3a1c16d5a
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-08-12 22:21:25 +0100

    Add combinations.py file

---
+++ python/combinations.py
@@ -0,0 +1,19 @@
+def combinations(n, k):
+    combos = []
+
+    if (k == 1):
+        return n
+    
+    for i in range(len(n)):
+        head = n[i:i+1]
+        tail = combinations(n[i+1:],k-1)
+
+        for j in range(len(tail)):
+            print("tail[j]", tail[j])
+            if (type(tail[j]) == int):
+                combo = head + [tail[j]]
+            else:
+                combo = head + tail[j]
+            combos.append(combo)
+            
+    return combos
