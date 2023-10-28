ce1b2f423f60c8387e3f04eef16b982ed3810734
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-10-28 22:22:57 +0100

    Update search.java file for performing linear search

--- java/search.java
+++ java/search.java
@@ -7,10 +7,21 @@ public class search {
         }
         return "Not found...";
     }
+
+    static int linearSearch2(int[] arr, int target) {
+        for (int index = 0; index <= arr.length - 1; index++) {
+            if (arr[index] == target) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] argc) {
         int[] arr = new int[]{23, 22, 24, 25};
         int target = 25;
 
         System.out.println(linearSearch(arr, target));
+        System.out.println(linearSearch2(arr, target));
     }
 }
